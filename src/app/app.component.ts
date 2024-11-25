85d735fcc572c372ca7e51c5503a58ea00624c38
swetha712 <swethamurugan2222@gmail.com>
2024-11-25 13:01:59 +0530

    code pushed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { IonicModule } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatButtonModule,MatCardModule,MatIconModule,IonicModule],
+  
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
